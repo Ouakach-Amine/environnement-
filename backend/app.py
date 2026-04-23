3171c34b330a29262f330907d0fee69a9338bfd4
Amine Ouakach <aminoukhouya86@gmail.com>
2026-04-23 22:09:01 +0100

    deux

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,35 @@
+from flask import Flask, request, jsonify
 from kafka import KafkaProducer
 import json
 import time
 
-while True:
+app = Flask(__name__)
+
+# Connexion à Kafka (nom du service Docker)
+producer = None
+
+while producer is None:
     try:
         producer = KafkaProducer(
             bootstrap_servers='kafka:9092',
             value_serializer=lambda v: json.dumps(v).encode('utf-8')
         )
-        print("Connected to Kafka ")
-        break
+        print("✅ Connected to Kafka")
     except:
-        print("Kafka not ready, retrying...")
+        print("⏳ Kafka not ready, retrying...")
         time.sleep(5)
 
-while True:
-    data = {
-        "player_id": 1,
-        "score": 80,
-        "reaction_time": 2.5
-    }
-
+@app.route('/send', methods=['POST'])
+def send_data():
+    data = request.json
+    
     producer.send('game_topic', data)
-    print("sent:", data)
+    
+    return jsonify({"status": "sent to Kafka", "data": data})
 
-    time.sleep(2)
+@app.route('/')
+def home():
+    return "Backend running"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
